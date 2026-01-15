f3258ebeecccbc0ab750778707937a1c1fa902d8
jonathan youngblood <jonathan.youngblood@gmail.com>
2026-01-14 19:57:20 -0600

    docs content cleanup

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,13 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
 import { docsSchema } from '@astrojs/starlight/schema';
 
 export const collections = {
 	docs: defineCollection({ schema: docsSchema() }),
+	fieldguide: defineCollection({
+		type: 'content',
+		schema: z.object({
+			title: z.string(),
+			description: z.string().optional(),
+		}),
+	}),
 };
